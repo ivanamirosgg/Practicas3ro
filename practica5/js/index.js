36dc7cfa47c6654f1ba3ae9837413879495d99c6
ivanamirosgg <102983934+ivanamirosgg@users.noreply.github.com>
2023-02-25 12:36:11 -0600

    Add files via upload
    practica 5 - CRUD

--- practica5/js/index.js
+++ practica5/js/index.js
@@ -2,7 +2,18 @@ let formulario = document.getElementById("formulario");
 let nombre= document.getElementById("nombre");
 let fecha = document.getElementById("fecha");
 let descripcion = document.getElementById("descripcion");
+
+
+let formularioEditar = document.getElementById("formularioEditar");
+let nombreEditar= document.getElementById("nombreEditar");
+let fechaEditar = document.getElementById("fechaEditar");
+let descripcionEditar = document.getElementById("descripcionEditar");
+
+
 let btnGuardar = document.getElementById("btnGuardar");
+let btnGuardarEditar = document.getElementById("btnGuardarEditar");
+let idTarea = document.getElementById("idTarea");
+
 let tareas = [];
 
 let agregarDatos = ()  => {
@@ -31,22 +42,27 @@ let mostrarTareas = () => {
     tareas.forEach( (tarea, indice) =>{
         listaTareas.innerHTML += `
         <div class='row' id=${indice}>
-            <div class='col-3 border p-3'>
+            <div class='col-3 border p-3 textoalineado'>
                 <strong>${tarea.nombre}</strong>
             </div>
-            <div class='col-3 border p-3'>
+            <div class='col-3 border p-3 textoalineado'>
                 <strong>${tarea.fecha}</strong>
             </div>
-            <div class='col-3 border p-3'>
+            <div class='col-2 border p-3 textoalineado'>
                 <strong>${tarea.descripcion}</strong>
             </div>
-            <div class='col-3 border p-3 text-center'>
+            <div class='col-2 border p-3 text-center'>
+                <button class='btn btn-success' onClick ="editarTarea(${indice});" data-bs-toggle="modal" data-bs-target="#exampleModalEditar"><i class="bi bi-pencil"></i> Editar</button>
+            </div>
+            <div class='col-2 border p-3 text-center'>
                 <button class='btn btn-danger' onClick ="borrarTarea(this,${indice});"><i class="bi bi-trash"></i> Borrar</button>
             </div>
         </div>
         `;
     });
+
 }
+
 
 formulario.addEventListener("submit",(e)=>{
     e.preventDefault();
@@ -55,3 +71,42 @@ formulario.addEventListener("submit",(e)=>{
     resetearFormulario();
     mostrarTareas();
 });
+
+formularioEditar.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let indice = idTarea.value;
+    tareas[indice].nombre = nombreEditar.value;
+    tareas[indice].fecha = fechaEditar.value;
+    tareas[indice].descripcion = descripcionEditar.value;
+    mostrarTareas();
+    cerrarModalEditar();
+    
+
+    
+});
+
+
+let borrarTarea = (boton, indice) => {
+    if(confirm("¿Estás seguro de eliminar de eliminar este registro?"))
+    {
+        boton.parentElement.parentElement.remove();
+        tareas.splice(indice,1);
+    }
+   
+
+}
+   
+let editarTarea = (indice) => {
+    nombreEditar.value = tareas[indice].nombre;
+    fechaEditar.value = tareas[indice].fecha;
+    descripcionEditar.value = tareas[indice].descripcion;
+    idTarea.value = indice;
+}
+
+let cerrarModalEditar = () => {
+    btnGuardarEditar.setAttribute("data-bs-dismiss", "modal");
+    btnGuardarEditar.click();
+}
+
+    
+
